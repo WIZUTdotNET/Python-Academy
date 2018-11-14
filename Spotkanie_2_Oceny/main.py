91fc63e3399d2905d20351eaa2b42585bb5802cb
Pawel Kalicki <kalicki.paul@icloud.com>
2018-11-14 11:05:38 +0100

    Changed condition

--- Spotkanie_2_Oceny/main.py
+++ Spotkanie_2_Oceny/main.py
@@ -2,9 +2,9 @@ import parsemod as pd
 import createPDF as cpdf
 import createExcel as cxlsx
 
-filename = "edziekanat_przyklad.txt"
+filename = "edziekanat_example.txt"
 
-if __name__ == 'main':
+if __name__ == "__main__":
     grades_list = pd.parse_edziekanat_grades(filename)
     pdf_file = cpdf.create_PDF_file(grades_list)
     excel_file = cxlsx.create_Excel_file(grades_list)
