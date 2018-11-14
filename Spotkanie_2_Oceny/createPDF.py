e4377b8aa64c3c8811180c478813e4732156ca11
Pawel Kalicki <kalicki.paul@icloud.com>
2018-11-14 11:01:43 +0100

    Added createPDF module

---
+++ Spotkanie_2_Oceny/createPDF.py
@@ -0,0 +1,40 @@
+"""
+Interesting pdf - creation :
+
+http://www.devshed.com/c/a/Python/Python-for-PDF-Generation/
+
+"""
+
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import cm, mm, inch, pica
+
+def create_PDF_file(handler):
+
+    # Create new pdf document
+    pdf = Canvas("test.pdf", pagesize=letter)
+
+    # Set font and draw title
+    pdf.setFont("Courier-Bold", 14)
+    pdf.setStrokeColorRGB(1, 0, 0)
+    pdf.drawString(inch * 1, inch * 10, lista_ocen[0][0])
+
+    # set font for rows with grades
+    pdf.setFont("Courier-Bold", 12)
+    pdf.setStrokeColorRGB(1, 0, 0)
+    # create handle used to add lines to pdf
+    table = pdf.beginText(inch * 1, inch * 9)
+
+    for x in handler[1:]:
+        # join lines with tab and add them to table
+        row = "    ".join(x)
+        table.textLine(row)
+
+    weighted_sum = [a * b for a, b in zip([float(i[2].replace(',', '.')) for i in handler[2:]],
+                                      [float(i[3].replace(',', '.')) for i in handler[2:]])]
+    table.textLine("Twoja srednia to:  {2.f}".format(str(sum(weighted_sum) / 30)))
+
+    # draw all lines to pdf
+    pdf.drawText(table)
+    pdf.showPage()
+    pdf.save()
