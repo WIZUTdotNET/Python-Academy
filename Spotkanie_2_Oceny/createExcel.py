db10aa6e9492f97fdf1f2a7ab53eff2d117a231a
Pawel Kalicki <kalicki.paul@icloud.com>
2018-11-15 15:42:14 +0100

    Added method of create Excel

--- Spotkanie_2_Oceny/createExcel.py
+++ Spotkanie_2_Oceny/createExcel.py
@@ -1,2 +1,28 @@
-def create_Excel_file(handler):
-    pass
+import xlwt
+import re
+
+def createExcelFile(table):
+
+    style0 = xlwt.easyxf('font: name Times New Roman, color-index red, bold on')
+
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet('A Test Sheet')
+
+    grades = []
+    column = 5
+
+    for j in range(len(table)):
+        for i in range(len(table[0])):
+            if i == 10 and j > 0:
+                ws.write(j,i,float(table[j][i]))
+            elif i == 5 and j > 0 or i == 6 and j > 0 or i == 7 and j > 0:
+                regex = re.sub('\d\d.\d\d.\d\d$', '', table[j][i])
+                try:
+                    #ws.write(j,i,float(regex))
+                    ws.cell(row=i, column=j).value = float(regex)
+                except:
+                    ws.write(j,i,regex)
+            else:
+                ws.write(j,i,table[j][i])
+
+    wb.save('raport.xls')
