6a63c7ed2f1dfcce6162b444c01949bd94837e7f
Pawel Kalicki <kalicki.paul@icloud.com>
2018-12-13 15:58:25 +0100

    Spotkanie 6

---
+++ Spotkanie_6_UX_ML/klasyfikator.py
@@ -0,0 +1,20 @@
+from sklearn import tree
+
+def sprawdzPlec(dim):
+    
+    # [height, weight, shoe size]
+    X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37],
+         [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40],
+         [171, 75, 42], [181, 85, 43]]
+
+    Y = ['male', 'male', 'female', 'female',
+         'female', 'male', 'female', 'male',
+         'female', 'male']
+
+    clf = tree.DecisionTreeClassifier()
+
+    clf = clf.fit(X, Y)
+
+    prediction = clf.predict([dim])
+
+    return(prediction)
---
+++ Spotkanie_6_UX_ML/main.py
@@ -0,0 +1,7 @@
+from klasyfikator import sprawdzPlec as sP
+
+if __name__ == "__main__":
+
+    lista = [190, 80, 45]
+    wynik = sP(lista)
+    print(wynik)
